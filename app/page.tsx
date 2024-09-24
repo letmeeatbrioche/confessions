9c8a3f9e2a615b34f1410b7fdad449628367eadd
Haillie W <hailliewilloughby@gmail.com>
2024-09-24 17:05:53 -0500

    Replace h2 with subheading component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import Footer from "@/components/Footer";
 import styles from "./page.module.css";
 import Confessions from "@/components/Confessions";
+import Subheading from "@/components/Subheading";
 
 export default function Home() {
 
   return (
     <main className={styles.main}>
       <h1 className="header">CONFíT<sup>9</sup></h1>
-      <h2 className="subheading">What&apos;s on your mind?</h2>
+      <Subheading />
       <Confessions />
       <Footer />
     </main>
